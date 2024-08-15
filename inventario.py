61723ad50e78a2aa108afa65917b5140d7957c2c
Jogo-Campo-Grande <danielzsanxhes@gmail.com>
2024-08-15 08:19:12 -0400

    Arrumei os comment

--- inventario.py
+++ inventario.py
@@ -1,11 +1,13 @@
+##Inv e atributo 
 inv={"Uma Pequena Faca":10,"Uma Nota De $100":100,}
 ess={"Fome":0,"Saude":100,"Doença":0,"Felicidade":100}
 mortes=0
 roubos=0
-
+#@ while/main
 while True:
-    print("Aqui perto tem um Mercado, aqui perto também tem uma Loja de Roupa")
+    print("")
     print("--"*30)
     print("-Devo ir aonde?"
           "\n...")
+    
     per=input("*")
